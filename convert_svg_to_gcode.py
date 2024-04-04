545201e12b9b0dcedd886cb6b8b0063616c48137
Stewart Kelly <stewkelly@icloud.com>
2024-04-04 16:23:40 +1100

    Update

--- convert_svg_to_gcode.py
+++ convert_svg_to_gcode.py
@@ -10,16 +10,15 @@ def convert_svg_to_gcode(svg_file_name, gcode_file_name):
     svg_file_path = os.path.join(script_directory, svg_file_name)
     gcode_file_path = os.path.join(script_directory, gcode_file_name)
 
-    # Instantiate a compiler, specifying the interface type, maximum_laser_power and speed at which the tool moves while 
-    # cutting. pass_depth controls how deep the laser cuts after every pass.
-    gcode_compiler = Compiler(interfaces.Gcode, 100, 900, 1)
+    # Instantiate a compiler, specifying the interface type, movement speed and drawing speed at which the tool moves while 
+    gcode_compiler = Compiler(interfaces.Gcode, 0, 100, 50)
 
     # Parse an svg file into geometric curves, and compile to gcode
     curves = parse_file(svg_file_path)
     gcode_compiler.append_curves(curves)
 
     # Compile the G-code and save it directly to a file
-    gcode_compiler.compile_to_file(gcode_file_path, passes=2)
+    gcode_compiler.compile_to_file(gcode_file_path)
 
 if __name__ == "__main__":
     convert_svg_to_gcode("drawing.svg", "drawing.gcode")
